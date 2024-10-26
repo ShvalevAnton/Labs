ec209e5ac58c7cd0bf5c6123622e94c08b352acf
Shvalev Anton <shvalev_as88@mail.ru>
2024-10-26 22:47:37 +0300

    Добавлена лаб. работа 3

---
+++ Lab3/Target shooting/Target shooting/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Target_shooting
+{  
+    internal class Program
+    {  
+        static void Main(string[] args)
+        {           
+            // Вариант мишени 2
+            // Координаты выстрела
+            int x = 0;
+            int y = 0;
+            // Набранная сумма очков после 3 выстрелов по мишени
+            int res = 0;
+            try
+            {
+                while (true)
+                {
+                    for (int i = 3; i > 0; i--)
+                    {
+                        Console.Write("Введите координату выстрела по х: ");
+                        x = Int32.Parse(Console.ReadLine());
+                        Console.Write("Введите координату выстрела по y: ");
+                        y = Int32.Parse(Console.ReadLine());
+
+                        // Стрельба в слепую
+                        double shift = new Random().Next(0, 5);
+                        Console.WriteLine($"Центр мишени смещен на {shift}");
+
+                        // Трудность при выстреле
+                        bool err = Convert.ToBoolean(new Random().Next(2));
+                        if (err)
+                        {
+                            Console.WriteLine($"Произошла осечка в оружии, осталось {i - 1} выстрела");
+                            Console.WriteLine("----------------------------------------------");
+                            continue;
+                        }
+
+                        if (x * x + y * y + shift < 1)
+                        {
+                            Console.WriteLine("Попали в область 1");
+                            res += 10;
+                        }
+                        else if (x * x + y * y + shift < 4)
+                        {
+                            Console.WriteLine("Попали в область 2");
+                            res += 5;
+                        }
+                        else if (x * x + y * y + shift <= 9)
+                        {
+                            Console.WriteLine("Попали в область 3");
+                            res += 1;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Промах");
+                        }
+                        Console.WriteLine("----------------------------------------------");
+                    }
+                    Console.WriteLine($"Результат выстрелов {res} ");
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
