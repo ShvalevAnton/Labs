7a03c7fc980a4bff2ce075255b08fac9499a08d4
Shvalev Anton <shvalev_as88@mail.ru>
2024-10-26 22:57:43 +0300

    Добавлена лаб. работа 3

--- Lab3/Target shooting/Target shooting/Program.cs
+++ Lab3/Target shooting/Target shooting/Program.cs
@@ -18,51 +18,50 @@ namespace Target_shooting
             int res = 0;
             try
             {
-                while (true)
+                
+                for (int i = 3; i > 0; i--)
                 {
-                    for (int i = 3; i > 0; i--)
+                    Console.Write("Введите координату выстрела по х: ");
+                    x = Int32.Parse(Console.ReadLine());
+                    Console.Write("Введите координату выстрела по y: ");
+                    y = Int32.Parse(Console.ReadLine());
+
+                    // Стрельба в слепую
+                    double shift = new Random().Next(4);
+                    Console.WriteLine($"Центр мишени смещен на {shift}");
+
+                    // Трудность при выстреле
+                    bool err = Convert.ToBoolean(new Random().Next(2));
+                    if (err)
                     {
-                        Console.Write("Введите координату выстрела по х: ");
-                        x = Int32.Parse(Console.ReadLine());
-                        Console.Write("Введите координату выстрела по y: ");
-                        y = Int32.Parse(Console.ReadLine());
-
-                        // Стрельба в слепую
-                        double shift = new Random().Next(0, 5);
-                        Console.WriteLine($"Центр мишени смещен на {shift}");
-
-                        // Трудность при выстреле
-                        bool err = Convert.ToBoolean(new Random().Next(2));
-                        if (err)
-                        {
-                            Console.WriteLine($"Произошла осечка в оружии, осталось {i - 1} выстрела");
-                            Console.WriteLine("----------------------------------------------");
-                            continue;
-                        }
-
-                        if (x * x + y * y + shift < 1)
-                        {
-                            Console.WriteLine("Попали в область 1");
-                            res += 10;
-                        }
-                        else if (x * x + y * y + shift < 4)
-                        {
-                            Console.WriteLine("Попали в область 2");
-                            res += 5;
-                        }
-                        else if (x * x + y * y + shift <= 9)
-                        {
-                            Console.WriteLine("Попали в область 3");
-                            res += 1;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Промах");
-                        }
+                        Console.WriteLine($"Произошла осечка в оружии, осталось {i - 1} выстрела");
                         Console.WriteLine("----------------------------------------------");
+                        continue;
                     }
-                    Console.WriteLine($"Результат выстрелов {res} ");
+
+                    if (x * x + y * y + shift < 1)
+                    {
+                        Console.WriteLine("Попали в область 1");
+                        res += 10;
+                    }
+                    else if (x * x + y * y + shift < 4)
+                    {
+                        Console.WriteLine("Попали в область 2");
+                        res += 5;
+                    }
+                    else if (x * x + y * y + shift <= 9)
+                    {
+                        Console.WriteLine("Попали в область 3");
+                        res += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Промах");
+                    }
+                    Console.WriteLine("----------------------------------------------");
                 }
+                Console.WriteLine($"Результат выстрелов {res} ");
+                
             }
             catch (FormatException e)
             {
