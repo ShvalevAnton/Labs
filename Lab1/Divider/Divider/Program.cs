dbdf14aa14c0751b084463e320c2694ebed8d9d7
Shvalev Anton <shvalev_as88@mail.ru>
2024-10-26 18:40:19 +0300

    lab 3.1.0

--- Lab1/Divider/Divider/Program.cs
+++ Lab1/Divider/Divider/Program.cs
@@ -10,12 +10,23 @@ namespace Divider
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the first integer");       
-            int i = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the second integer");
-            int j = Int32.Parse(Console.ReadLine());
-            int k = i / j;
-            Console.WriteLine($"The result of dividing {i} by {j} is {k}");
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                int i = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the second integer");
+                int j = Int32.Parse(Console.ReadLine());
+                int k = i / j;
+                Console.WriteLine($"The result of dividing {i} by {j} is {k}");
+            }
+            catch(FormatException e) 
+            {
+                Console.WriteLine($"An format exception was thrown: {e.Message} ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An exception was thrown: {e.Message} ");
+            }
         }
     }
 }
--- Lab3/WhatDay1/WhatDay1/WhatDay.cs
+++ Lab3/WhatDay1/WhatDay1/WhatDay.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,30 +25,87 @@ namespace WhatDay1
     }
     internal class WhatDay
     {
+        // Don't modify anything below here
+        static System.Collections.ICollection DaysInMonths
+            //= new char [4] {'d','f','g','r'}; 
+            = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        static System.Collections.ICollection DaysInLeapMonths
+            = new int[12] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         static void Main(string[] args)
         {
-            Console.Write("Please enter a day number between 1 and 365: ");
-            int dayNum = int.Parse(Console.ReadLine());            
-            int monthNum = 0;
-            int DaysInMonths = DateTime.DaysInMonth(dayNum, monthNum);
-            foreach (int daysInMonth in DaysInMonths)
+            try
             {
-                if (dayNum <= daysInMonth)
+                while (true)
                 {
-                    break;
-                }
-                else
-                {
-                    dayNum -= daysInMonth;
-                    monthNum++;
+                    Console.WriteLine("Please enter the year: ");
+                    int yearNum = int.Parse(Console.ReadLine());
+                    // Проверка на высокосный год
+                    bool isLeapYear = (yearNum % 4 == 0) && (yearNum % 100 != 0 || yearNum % 400 == 0);
+                    int maxDayNum = isLeapYear ? 366 : 365;
+
+                    if (isLeapYear)
+                    {
+                        Console.WriteLine(" IS a leap year"); // высокосный год
+                    }
+                    else
+                    {
+                        Console.WriteLine(" is NOT a leap year");// не высокосный год
+                    }
+
+                    Console.Write($"Please enter a day number between 1 and {maxDayNum}: ");
+                    int dayNum = int.Parse(Console.ReadLine());
+                    if (dayNum < 1 || dayNum > maxDayNum)
+                    {
+                        throw new ArgumentOutOfRangeException("Day out of range");
+                    }
+                    int monthNum = 0;
+
+                    if (isLeapYear)
+                    {
+                        // считаем месяци и день для высокосного года
+                        foreach (int daysInMonth in DaysInLeapMonths)
+                        {
+                            if (dayNum <= daysInMonth)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                // Считает дни в году
+                                dayNum -= daysInMonth;
+                                // Считает месяц в году
+                                monthNum++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // считаем месяци и день для не высокосного года
+                        foreach (int daysInMonth in DaysInMonths)
+                        {
+                            if (dayNum <= daysInMonth)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                // Считает дни в году
+                                dayNum -= daysInMonth;
+                                // Считает месяц в году
+                                monthNum++;
+                            }
+                        }
+                    }
+                    MonthName temp = (MonthName)monthNum;
+                    string monthName = temp.ToString();
+                    Console.WriteLine("{0} {1}", dayNum, monthName);
                 }
             }
-            MonthName temp = (MonthName)monthNum;
-            string monthName = temp.ToString();
-            Console.WriteLine("{0} {1}", dayNum, monthName);
-
-
-
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
