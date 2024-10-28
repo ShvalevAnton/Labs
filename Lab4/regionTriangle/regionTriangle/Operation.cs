e712578e954f7fc0a4adea0e19693b109a66438a
Shvalev Anton <shvalev_as88@mail.ru>
2024-10-28 23:06:46 +0300

    добавлена лабораторная работа 4.4

---
+++ Lab4/regionTriangle/regionTriangle/Operation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regionTriangle
+{
+    internal class Operation
+    {
+        // Статический открытый метод для расчета площади треугольника по формуле Герона
+        static public double CalcSquTriang(double sA, double sB, double sC)
+        {           
+            if (Operation.isTriang(sA, sB, sC))
+            {
+                throw new Exception("Введенные точки не образуют треугольник.");
+            }
+            // Периметр треугольника
+            double P = (sA + sB + sC) / 2.0;
+            // Площадь треугольника
+            double squT = Math.Sqrt(P * (P - sA) * (P - sB) * (P - sC));           
+            return squT;
+        }
+
+        // Статический открытый метод для расчета площади равностороннего треугольника по формуле Герона
+        static public double CalcSquTriang(double sA)
+        {
+            // Периметр треугольника
+            double P = (sA * 3) / 2.0;
+            // Площадь треугольника
+            double squT = Math.Sqrt(P * (P - sA) * 3);            
+            return squT;
+        }
+        // Проверка на треугольник
+        static bool isTriang(double sA, double sB, double sC)
+        {
+            return (sA + sB <= sC) || (sA + sC <= sB) || (sC + sB <= sA);
+        }
+
+    }
+}
---
+++ Lab4/regionTriangle/regionTriangle/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace regionTriangle
+{
+    internal class Program
+    {  
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Необходимо определить площадь треугольника.\nУкажите тип треугольника: 1 — равнобедренный, 2 — разносторонний.");              
+
+                int typeTriang = Int32.Parse(Console.ReadLine());
+                if (typeTriang != 1 && typeTriang != 2)
+                {
+                    throw new Exception("Выбран не верный тип треугольника!");
+                }
+
+                int sideA = 0;
+                int sideB = 0;
+                int sideC = 0; 
+                if (typeTriang == 1)
+                {
+                    // Длинна стороны А треугольника
+                    Console.WriteLine("Пожалуйста, напишите длину стороны равнобедренного треугольника.");
+                    sideA = Int32.Parse(Console.ReadLine());
+                    double squTriang = Operation.CalcSquTriang(sideA);
+                    Console.WriteLine($"Площадь треугольника = {squTriang}.");
+                }
+                else if (typeTriang == 2)
+                {
+                    // Длинна стороны А треугольника
+                    Console.WriteLine("Пожалуйста, напишите длину стороны А разностороннего треугольника.");
+                    sideA = Int32.Parse(Console.ReadLine());
+                    // Длинна стороны B треугольника
+                    Console.WriteLine("Пожалуйста, напишите длину стороны В разностороннего треугольника.");
+                    sideB = Int32.Parse(Console.ReadLine());
+                    // Длинна стороны C треугольника
+                    Console.WriteLine("Пожалуйста, напишите длину стороны С разностороннего треугольника.");
+                    sideC = Int32.Parse(Console.ReadLine());
+
+                    double squTriang = Operation.CalcSquTriang(sideA, sideB, sideC);                    
+                    Console.WriteLine($"Площадь треугольника = {squTriang}.");
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
