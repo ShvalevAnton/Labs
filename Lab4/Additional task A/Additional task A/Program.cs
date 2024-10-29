532049d798ee54bb7b84c50bb421c310be4b91d3
Shvalev Anton <shvalev_as88@mail.ru>
2024-10-29 21:01:29 +0300

    добавлена дополнительное задание А

---
+++ Lab4/Additional task A/Additional task A/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_task_A
+{
+    internal class Program
+    {
+        // Статический метод расчета 
+        static public double calcSquRoot(double target, ref double x)
+        {            
+            double oldx;
+            do
+            {
+                oldx = x;
+                x = (x + target / x) / 2;
+            }
+            while (oldx != x);
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            double target = 2023;
+            // Объявление и инициализация переменной, для которой требуется определить квадратный корень.
+            double x = 1;
+
+            // Вызов метода с передачей результата другой переменной.
+            double y = calcSquRoot(target,ref x);
+
+            // Вывод результата (x) и проверочного значения (x*x) на экран.
+            Console.WriteLine(x);
+            Console.WriteLine(x * x);
+        }
+    }    
+}
