ab23b673453ff133cf91050d6901a913d5315147
Shvalev Anton <shvalev_as88@mail.ru>
2024-11-04 22:46:32 +0300

    добавлена лабораторная работа 7

---
+++ Lab7/CopyFileUpper/CopyFileUpper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFileUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sFrom; // Имя входного файла
+            string sTo; // Имя выходного файла
+            StreamReader srFrom; // хранится ссылка на входной файл
+            StreamWriter srTo;  // хранится ссылка на выходной файл
+
+            // Запрос имени входного файла
+            Console.Write("Копировать из:");
+            sFrom = Console.ReadLine();
+            // Запрос имени выходного файла
+            Console.Write("Копировать в:");
+            sTo = Console.ReadLine();
+            Console.WriteLine("Копировать из {0} в {1}", sFrom, sTo);
+            try
+            {
+                srFrom = new StreamReader(sFrom);
+                srTo = new StreamWriter(sTo);
+                while (srFrom.Peek() != -1)
+                {
+                    string sBuffer = srFrom.ReadLine();
+                    sBuffer = sBuffer.ToUpper();
+                    srTo.WriteLine(sBuffer);
+                }
+                srTo.Close();
+                srFrom.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Неожиданное исключение");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Lab7/sortObjClass/sortObjClass/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Book: IComparable
+{
+    String author;
+    String title;
+    int pages; 
+    int year;
+
+    public Book (String author, String title, int pages, int year)
+    {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.year = year;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n", author, title, year, pages);
+    }
+    //public override string ToString() => $"\nКнига:\n Автор: {author}\n Название: {title}\n Год издания: {year}\n {pages} стр.\n";
+
+    int IComparable.CompareTo(object obj)
+    {
+        Book it = (Book)obj;
+        if (this.year == it.year) return 0;
+        else if (this.year > it.year) return 1;
+        else return -1;
+    }
+}
+
+namespace sortObjClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] arrBook = new Book [5]{ 
+                new Book("Иванов", "У реки", 10, 2020), 
+                new Book("Петров", "Городки", 20, 2021), 
+                new Book("Степанов", "Космос", 30, 2022), 
+                new Book("Сабитов", "Кино", 40, 2023),
+                new Book("Лебедев", "Испытания", 50, 2024)
+            }; 
+            Array.Sort(arrBook);
+            foreach (Book book in arrBook)
+            {
+                book.Show();
+            }
+        }
+    }
+}
