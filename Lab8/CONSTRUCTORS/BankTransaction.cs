12d3ca2be02e100473b81cd7e6e6d064ff3697ea
Shvalev Anton <shvalev_as88@mail.ru>
2024-11-06 21:21:43 +0300

    лабор. работа 8 упр 1 -3

---
+++ Lab8/CONSTRUCTORS/BankTransaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+internal class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;   
+
+    public BankTransaction(decimal tranAmount)
+    {
+        this.amount = tranAmount;
+        when = DateTime.Now;
+    }
+
+    public decimal Amount()
+    {
+        return amount;
+    }
+    public DateTime When()
+    {
+        return when;
+    }
+
+
+}
+
