fce92c9224d6211eeac107930fa2d047fdee519f
Shvalev Anton <shvalev_as88@mail.ru>
2024-11-09 14:22:04 +0300

    лабораторная работа 9 упражнения 1-4

---
+++ Lab9/MyClass/MyClass/Book.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass 
+{
+    internal class Book:Item // Класс книга
+    {
+        private String author; // автор
+        private String title; // название
+        private String publisher; // издательство
+        private int pages; // кол-во страниц
+        private int year; // год издания
+
+        private static double price = 9; //стоимость аренды за любую книгу
+
+        private bool returnSrok; // учет возвращения книги в срок
+
+        public Book() { }    // Конструктор по умолчанию
+
+        public Book(String author, String title, String publisher, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.pages = pages;
+            this.year = year;
+        }
+        public Book(String author, String title, String publisher, int pages, int year, long invNumber, bool taken) : base(invNumber, taken)
+        {
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.pages = pages;
+            this.year = year;
+        }
+
+
+        static Book() // Статический конструктор
+        {
+            price = 10;
+        }
+        public static void SetPrice(double price)
+        {
+            Book.price = price; // Статический метод, реализующий возможность изменить стоимость аренды
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n Стоимость аренды: {4}", author, title, year, pages, Book.price);
+            base.Show();
+        }
+
+        public double PriceBook(int s)
+        {
+            double cust = s * price;
+            return cust;
+        }       
+        public void ReturnSrok() // Метод устанавливающий, что книга сдана в срок:
+        {
+            returnSrok = true;
+        }
+        public override void Return() // операция "вернуть"
+        {
+            if (returnSrok == true)
+                taken = true;
+            else
+                taken = false;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Lab9/MyClass/MyClass/Item.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    abstract class Item
+    {        
+        protected long invNumber;   // инвентарный номер — целое число       
+        protected bool taken;    // хранит состояние объекта - взят ли на руки
+
+        public Item()
+        {
+            this.taken = true;
+        }
+        public Item(long invNumber, bool taken)
+        {
+            this.invNumber = invNumber;
+            this.taken = taken;
+        }
+
+        // истина, если этот предмет имеется в библиотеке
+        public bool IsAvailable()
+        {
+            if (taken == true)
+                return true;
+            else
+                return false;
+        }
+        // инвентарный номер
+        public long GetInvNumber()
+        {
+            return invNumber;
+        }
+        // операция "взять"
+        protected void Take()
+        {
+            taken = false;
+        }
+        // операция "вернуть"
+        abstract public void Return();
+        // вывод на печать
+        public virtual void Show()
+        {
+            Console.WriteLine("Состояние единицы хранения:\n Инвентарный номер: {0}\n Наличие: {1}", invNumber, taken);
+        }
+        public void TakeItem()
+        {
+            if (this.IsAvailable())
+                this.Take();
+        }
+    }
+}
