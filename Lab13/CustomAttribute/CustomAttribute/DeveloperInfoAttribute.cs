95dc8e6af2cdf290e52eb311f7ea61d7683adf65
Shvalev Anton <shvalev_as88@mail.ru>
2024-11-10 21:26:06 +0300

    +лаб. раб.13 упраж. 2

---
+++ Lab13/CustomAttribute/CustomAttribute/DeveloperInfoAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Struct, AllowMultiple = true)]
+    public class DeveloperInfoAttribute : System.Attribute
+    {
+        private string developerName;
+        private string dateCreated;
+        // Constructor. Developer name is the only
+        // mandatory parameter for this attribute.
+        public DeveloperInfoAttribute(string developerName)
+        {
+            this.developerName = developerName;
+        }
+        public string Developer
+        {
+            get
+            {
+                return developerName;
+            }
+        }
+        // Optional parameter
+        public string Date
+        {
+            get
+            {
+                return dateCreated;
+            }
+            set
+            {
+                dateCreated = value;
+            }
+        }
+    }
+}
