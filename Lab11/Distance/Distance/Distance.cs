ecb1ae82af2c597d81298d7f67be32d52da80f20
Shvalev Anton <shvalev_as88@mail.ru>
2024-11-10 16:28:27 +0300

    + лаб. раб.11 контр. упр 3

---
+++ Lab11/Distance/Distance/Distance.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance
+{
+    internal class Distance
+    {
+        public int foot;   // фут
+        public int inch;   // дюйм
+
+        public Distance()
+        {
+            foot = 4;
+            inch = 9;
+        }
+        public Distance(int foot, int inch)
+        {
+            this.foot = foot;
+            this.inch = inch;
+        }
+
+        public static Distance operator + (Distance dist1, Distance dist2)
+        {
+            // Переводим все данные в дюймы и считаем сумму двух дистанций в дюймах
+            int sum = dist1.foot * 12 + dist1.inch + dist2.foot * 12 + dist2.inch;
+            // Подсчитываем количество футов
+            int foot = sum / 12;
+            // Подсчитываем количество дюймов
+            int inch = sum % 12;
+
+            // Возвращаем область памяти, где хранятся данные
+            return new Distance(foot, inch);
+        }
+
+        public static Distance operator -(Distance dist1, Distance dist2)
+        {
+            // Переводим все данные в дюймы и считаем сумму двух дистанций в дюймах
+            int dif = dist1.foot * 12 - dist1.inch - dist2.foot * 12 - dist2.inch;
+            // Подсчитываем количество футов
+            int foot = dif / 12;
+            // Подсчитываем количество дюймов
+            int inch = dif % 12;
+            
+            // Возвращаем область памяти, где хранятся данные
+            return new Distance(foot, inch);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            // Локальная объектная переменная
+            Distance d = (Distance) obj;
+            
+            // Переводим все данные в дюймы и сравниваем дистанции
+            int dist1 = foot * 12 + inch;
+            int dist2 = d.foot * 12 + d.inch;
+
+            return dist1 == dist2;          
+        }
+
+        public static bool operator ==(Distance dist1, Distance dist2)
+        {
+            return dist1.Equals(dist2);
+        }
+
+        public static bool operator !=(Distance dist1, Distance dist2)
+        {
+            return !(dist1 == dist2);
+        }
+
+        public override string ToString() => $"Сумма первой и второй дистанции равна: {foot}\'-{inch}\"";
+    }
+}
---
+++ Lab11/Distance/Distance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Distance
+{
+    
+    internal class Program
+    {
+        // Метод для ввода данных
+        static Distance enterData()
+        {
+            Console.Write($"Введите количество футов расстояния: ");
+            int Foot = Int32.Parse(Console.ReadLine());
+            Console.Write($"Введите количество дюймов расстояния: ");
+            int Inch = Int32.Parse(Console.ReadLine());
+
+            // Возвращаем область памяти, где хранятся данные 
+            return new Distance(Foot, Inch); 
+        }        
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Дистанция 1
+                Distance dist1 = enterData();
+                // Дистанция 2
+                Distance dist2 = new Distance();
+                // Сумма дистанции 1 и 2
+                Distance distSum = dist1 + dist2;
+                // Сумма дистанции 1 и 2
+                Distance distDif = dist1 - dist2;
+
+                Console.WriteLine(dist1);
+                Console.WriteLine(dist2);
+                Console.WriteLine(distSum);
+                Console.WriteLine(distDif);
+
+                Console.WriteLine("Дистанция 1 {0} Дистанция 2", dist1.Equals(dist2) ? "равно" : "не равно");
+                Console.WriteLine("Дистанция 1 {0} Дистанция 2", dist1 == dist2 ? "равно" : "не равно");
+                Console.WriteLine("Дистанция 1 {0} Дистанция 2", dist1 != dist2 ? "не равно" : "равно");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetType());
+            }            
+        }
+    }
+}
